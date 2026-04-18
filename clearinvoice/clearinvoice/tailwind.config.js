dcc3eddf54bafc8a50bbdf371a4ed63821df1987
DKTR N9NE <135451135+dktrn9ne@users.noreply.github.com>
2026-04-18 13:19:14 -0500

    Add files via upload

---
+++ clearinvoice/clearinvoice/tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        switzer: ['Switzer', 'sans-serif'],
+        geist: ['Geist', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
